9e1cc2e9b329620e2af3550c06970eeaa2afb020
fiandwicahya <fiandwicahya@gmail.com>
2018-07-11 14:24:52 +0700

    first commit

---
+++ www/peta/data/TempatIbadah_1.js
@@ -0,0 +1,23 @@
+var json_TempatIbadah_1 = {
+"type": "FeatureCollection",
+"name": "TempatIbadah_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Masjid": 104.0, "Gereja": 1.0, "Vihara": 0.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.327749034054293, -6.969520401337068 ] } },
+{ "type": "Feature", "properties": { "Masjid": 247.0, "Gereja": 14.0, "Vihara": 0.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.320655087534178, -7.003171859071055 ] } },
+{ "type": "Feature", "properties": { "Masjid": 109.0, "Gereja": 15.0, "Vihara": 1.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.318512942227784, -7.055729982354986 ] } },
+{ "type": "Feature", "properties": { "Masjid": 317.0, "Gereja": 4.0, "Vihara": 2.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.369884126060683, -7.068249281978426 ] } },
+{ "type": "Feature", "properties": { "Masjid": 118.0, "Gereja": 17.0, "Vihara": 1.0, "Pura": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 110.396892226647921, -7.018619477534733 ] } },
+{ "type": "Feature", "properties": { "Masjid": 112.0, "Gereja": 21.0, "Vihara": 2.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.412928286371582, -7.003959871281076 ] } },
+{ "type": "Feature", "properties": { "Masjid": 99.0, "Gereja": 18.0, "Vihara": 17.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.408919271440681, -6.989509236981186 ] } },
+{ "type": "Feature", "properties": { "Masjid": 160.0, "Gereja": 29.0, "Vihara": 0.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.407653266725646, -6.965633297405216 ] } },
+{ "type": "Feature", "properties": { "Masjid": 294.0, "Gereja": 6.0, "Vihara": 0.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.459770460827613, -6.968565495895686 ] } },
+{ "type": "Feature", "properties": { "Masjid": 126.0, "Gereja": 22.0, "Vihara": 6.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.423056324091817, -6.984482824611508 ] } },
+{ "type": "Feature", "properties": { "Masjid": 116.0, "Gereja": 8.0, "Vihara": 0.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.43318436181201, -6.989090371347965 ] } },
+{ "type": "Feature", "properties": { "Masjid": 327.0, "Gereja": 17.0, "Vihara": 3.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.453229436466614, -7.006682404456896 ] } },
+{ "type": "Feature", "properties": { "Masjid": 94.0, "Gereja": 12.0, "Vihara": 1.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.416620800123724, -7.022912560563217 ] } },
+{ "type": "Feature", "properties": { "Masjid": 308.0, "Gereja": 20.0, "Vihara": 2.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.451646930572821, -7.055999381225686 ] } },
+{ "type": "Feature", "properties": { "Masjid": 237.0, "Gereja": 33.0, "Vihara": 1.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.410290776548592, -7.076520398861248 ] } },
+{ "type": "Feature", "properties": { "Masjid": 198.0, "Gereja": 50.0, "Vihara": 4.0, "Pura": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 110.375430594327227, -6.986492858525364 ] } }
+]
+}
